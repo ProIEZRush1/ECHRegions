16e7ccea64ce6b42c6b56d611d6cc0635c58b880
ProIEZRush1 <edumaucherni@gmail.com>
2024-01-13 22:58:51 -0600

    Optimized getRegion

--- src/main/kotlin/com/proiezrush/echregions/database/mysql/MySQLDatabaseManager.kt
+++ src/main/kotlin/com/proiezrush/echregions/database/mysql/MySQLDatabaseManager.kt
@@ -8,7 +8,8 @@ import java.sql.Connection
 import java.sql.Statement
 
 class MySQLDatabaseManager(private val connection: Connection) : DatabaseManagerImpl {
-    override fun createRegion(name: String, position1: Position, position2: Position): Region? {
+
+    override fun createRegion(name: String, position1: Position, position2: Position): Region {
         val region = Region(name, position1, position2, listOf())
 
         this.connection.let { conn ->
@@ -40,43 +41,30 @@ class MySQLDatabaseManager(private val connection: Connection) : DatabaseManager
 
     override fun getRegion(regionId: Int): Region? {
         this.connection.let { conn ->
-            val stmt = conn.createStatement()
-
-            var regionName: String? = null
-
-            // First get region, then get positions, then get whitelisted players
-            val regionSet = stmt.executeQuery("SELECT * FROM ech_regions WHERE id = $regionId")
-            while (regionSet.next()) {
-                regionName = regionSet.getString("name")
-            }
-
-            var position1: Position? = null
-            var position2: Position? = null
-
-            val positionsSet = stmt.executeQuery("SELECT * FROM ech_positions WHERE regionId = $regionId")
-            while (positionsSet.next()) {
-                val positionType = positionsSet.getInt("type")
-                val world = positionsSet.getString("world")
-                val x = positionsSet.getDouble("x")
-                val y = positionsSet.getDouble("y")
-                val z = positionsSet.getDouble("z")
-
-                if (positionType == 1) {
-                    position1 = Position(1, world, x, y, z)
-                }
-                else if (positionType == 2) {
-                    position2 = Position(2, world, x, y, z)
-
-                }
-            }
-
             val whitelistedPlayers = mutableListOf<WPlayer>()
 
-            val whitelistedPlayersSet = stmt.executeQuery("SELECT * FROM ech_whitelisted_players WHERE regionId = $regionId")
-            while (whitelistedPlayersSet.next()) {
-                val uuid = whitelistedPlayersSet.getString("uuid")
+            // Get region
+            val regionQuery = "SELECT * FROM ech_regions WHERE id = ?"
+            val regionName = conn.prepareStatement(regionQuery).use { regionStmt ->
+                regionStmt.setInt(1, regionId)
+                regionStmt.executeQuery().use { regionSet ->
+                    if (regionSet.next()) regionSet.getString("name") else null
+                }
+            }
 
-                whitelistedPlayers.add(WPlayer(uuid))
+            // Get positions
+            val position1 = getPositionByType(conn, regionId, 1)
+            val position2 = getPositionByType(conn, regionId, 2)
+
+            // Get whitelisted players
+            val whitelistedPlayersQuery = "SELECT * FROM ech_whitelisted_players WHERE regionId = ?"
+            conn.prepareStatement(whitelistedPlayersQuery).use { whitelistedPlayersStmt ->
+                whitelistedPlayersStmt.setInt(1, regionId)
+                val whitelistedPlayersSet = whitelistedPlayersStmt.executeQuery()
+                while (whitelistedPlayersSet.next()) {
+                    val uuid = whitelistedPlayersSet.getString("uuid")
+                    whitelistedPlayers.add(WPlayer(uuid))
+                }
             }
 
             if (regionName == null || position1 == null || position2 == null) {
@@ -87,19 +75,36 @@ class MySQLDatabaseManager(private val connection: Connection) : DatabaseManager
         }
     }
 
+    private fun getPositionByType(conn: Connection, regionId: Int, type: Int): Position? {
+        val positionQuery = "SELECT * FROM ech_positions WHERE regionId = ? AND type = ?"
+        return conn.prepareStatement(positionQuery).use { positionStmt ->
+            positionStmt.setInt(1, regionId)
+            positionStmt.setInt(2, type)
+            positionStmt.executeQuery().use { positionsSet ->
+                if (positionsSet.next()) {
+                    val world = positionsSet.getString("world")
+                    val x = positionsSet.getDouble("x")
+                    val y = positionsSet.getDouble("y")
+                    val z = positionsSet.getDouble("z")
+                    Position(type, world, x, y, z)
+                } else null
+            }
+        }
+    }
+
     override fun deleteRegion(regionId: Int) {
-        TODO("Not yet implemented")
+
     }
 
     override fun addWhitelistedPlayer(whiteListedPlayer: WPlayer): WPlayer? {
-        TODO("Not yet implemented")
+
     }
 
     override fun removeWhitelistedPlayer(whiteListedPlayer: WPlayer) {
-        TODO("Not yet implemented")
+
     }
 
     override fun getWhitelistedPlayers(regionId: Int): List<WPlayer?> {
-        TODO("Not yet implemented")
+
     }
 }
